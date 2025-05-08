7957368754fc4e7bcca5f9c9fbc660d444e15fc6
AlcilenySilva <capic.2023118tads0045@aluno.ifpi.edu.br>
2025-05-08 09:17:50 -0300

    retirando a senha da internet

--- utils/wifi-connection/wifi-connect.h
+++ utils/wifi-connection/wifi-connect.h
@@ -6,8 +6,8 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
-#define WIFI_SSID "MAMBEE"
-#define WIFI_PASSWORD "1fp1mamb33"
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
 
 void init_wifi(void);
 
